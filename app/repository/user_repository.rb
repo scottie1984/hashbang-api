5d5986fc0ae0d9729ab2295cb1c1d388f8fb28f4
Stephen Scott <s.p.scott@kainos.com>
2014-06-26 20:33:28 +0100

    modified rake file

--- app/repository/user_repository.rb
+++ app/repository/user_repository.rb
@@ -5,7 +5,7 @@ db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
   password = db_parts[4]
   host = db_parts[5]
   db = db_parts[7]
-  conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
+  $conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
 
 class UserRepository
 
