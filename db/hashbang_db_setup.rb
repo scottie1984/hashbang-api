b42973707f94cce91b103fda9e9940b94f30d989
Stephen Scott <s.p.scott@kainos.com>
2014-06-26 21:11:00 +0100

    modified rake file

--- db/hashbang_db_setup.rb
+++ db/hashbang_db_setup.rb
@@ -4,7 +4,12 @@ module HashBangDB
   
   def self.setup()
     
-  conn = PGconn.open(:dbname => 'copper', :user => 'postgres')
+    db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+      username = db_parts[3]
+      password = db_parts[4]
+      host = db_parts[5]
+      db = db_parts[7]
+      conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
     conn.exec(
       <<-SQL
       CREATE TABLE users (
