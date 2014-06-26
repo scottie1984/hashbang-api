b3f5cbd466c70c325ba74b9e02e122668224b1db
Stephen Scott <s.p.scott@kainos.com>
2014-06-26 21:19:34 +0100

    modified rake file

--- db/hashbang_db_setup.rb
+++ db/hashbang_db_setup.rb
@@ -136,7 +136,12 @@ module HashBangDB
   end
 
   def self.seed_tags(tags)
-    conn = PGconn.open(:dbname => 'hashbang', :user => 'postgres')
+    db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+      username = db_parts[3]
+      password = db_parts[4]
+      host = db_parts[5]
+      db = db_parts[7]
+      conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
     insert = <<-SQL
       INSERT INTO tags
       VALUES (DEFAULT, $1, $2, $3, $4, $5)
@@ -147,7 +152,12 @@ module HashBangDB
   end
 
   def self.seed_tag_objects(tag_objects)
-    conn = PGconn.open(:dbname => 'hashbang', :user => 'postgres')
+    db_parts = ENV['DATABASE_URL'].split(/\/|:|@/)
+      username = db_parts[3]
+      password = db_parts[4]
+      host = db_parts[5]
+      db = db_parts[7]
+      conn = PGconn.open(:host =>  host, :dbname => db, :user=> username, :password=> password)
     insert = <<-SQL
       INSERT INTO tag_objects
       VALUES (DEFAULT, $1, $2, $3)
