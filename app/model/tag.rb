af275f119dbb48290bcf3a36d89bd35de392ef2d
Stephen Scott <s.p.scott@kainos.com>
2014-06-28 17:32:38 +0100

    Using amazon s3 for images

--- app/model/tag.rb
+++ app/model/tag.rb
@@ -22,31 +22,31 @@ class Tag
   
   def self.returnJSON(currentObject, previousObject, nextObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "original_file_name" => previousObject.original_file_name, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}/download"}, "next" => {"id" => nextObject.id, "original_file_name" => nextObject.original_file_name,  "type" => nextObject.type, "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}", "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}"}
+      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "original_file_name" => previousObject.original_file_name, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name}"}, "next" => {"id" => nextObject.id, "original_file_name" => nextObject.original_file_name,  "type" => nextObject.type, "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}_medium.jpg", "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name}"}
       }].to_json
   end
   
   def self.returnJSONNoNext(currentObject, previousObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
+      "current"       => JSON.parse(currentObject.to_json), "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
       }].to_json
   end
   
   def self.returnJSONNoPrevious(currentObject, nextObject)
     [{
-      "current"       => JSON.parse(currentObject.to_json), "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}", "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => nextObject.original_file_name}
+      "current"       => JSON.parse(currentObject.to_json), "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name}", "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => nextObject.original_file_name}
       }].to_json
   end
   
   def self.returnJSONPreviousRandomTag(previousObject, randomTag)
     [{
-      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
+      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"type" => 'end'}
       }].to_json
   end
   
   def self.returnJSONPreviousRandomTagWithNext(previousObject, randomTag, nextObject)
     [{
-      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}", "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => nextObject.original_file_name}
+      "randomTag" => randomTag, "previous" => {"id" => previousObject.id, "type" => previousObject.type, "overallScore" => previousObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{previousObject.file_name}","file_name_medium" => "#{CONFIG['backend_url']}/#{previousObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => previousObject.original_file_name}, "next" => {"id" => nextObject.id, "type" => nextObject.type, "overallScore" => nextObject.overallScore, "file_name" => "#{CONFIG['backend_url']}/#{nextObject.file_name}", "file_name_medium" => "#{CONFIG['backend_url']}/#{nextObject.file_name.split(".")[0]}_medium.jpg", "original_file_name" => nextObject.original_file_name}
       }].to_json
   end
   
