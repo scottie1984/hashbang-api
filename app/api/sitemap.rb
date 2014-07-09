d1d84f57feb4dd46081091b3aea3c90acd62867d
Stephen Scott <s.p.scott@kainos.com>
2014-07-09 23:06:04 +0100

    Finished sitemap endpoint

--- app/api/sitemap.rb
+++ app/api/sitemap.rb
@@ -18,13 +18,52 @@ module SocialChallenges
       xml = Builder::XmlMarkup.new( :indent => 2 )
       xml.instruct! :xml, :encoding => "UTF-8"
       tags = TagRepository.all('tag')
+      users = TagRepository.all('user')
       xml.urlset(:xmlns=>'http://www.sitemaps.org/schemas/sitemap/0.9') do |p|
         
-      tags.each { |tag |
         p.url {
-          p.loc "http://#{CONFIG['frontend_url']}/#!/rate/tag/#{tag['tagname']}"
+          p.loc "http://#{CONFIG['frontend_url']}"
         }
-      }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/recent"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/random"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/tagsearch"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/feedback"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/signup"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/login"
+        }
+        
+        p.url {
+          p.loc "http://#{CONFIG['frontend_url']}/#!/upload"
+        }
+        
+        tags.each { |tag |
+          p.url {
+            p.loc "http://#{CONFIG['frontend_url']}/#!/rate/tag/#{tag['tagname']}"
+          }
+        }
+        
+        users.each { |user |
+          p.url {
+            p.loc "http://#{CONFIG['frontend_url']}/#!/rate/user/#{user['tagname']}"
+          }
+        }
       end
       xml
     end
